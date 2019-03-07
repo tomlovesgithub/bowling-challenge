e9e928fa46db0f5cd826dd5d0acf7082c41e9d35
Tom Russell <TR115251@hotmail.co.uk>
2019-03-07 15:44:49 +0000

    Feature: Game score held, gutter game feature, frames attr reduced by frames func

--- scorecard.js
+++ scorecard.js
@@ -1,7 +1,15 @@
 class Scorecard {
+
   constructor() {
     this.frames = 10;
+    this.total = 0;
   }
+
+  frame(roll1,roll2) {
+    this.total =+ (roll1 + roll2)
+    this.frames -= 1 
+  }
+
 }
 
 module.exports = Scorecard;
--- scorecard.test.js
+++ scorecard.test.js
@@ -2,20 +2,47 @@ const Scorecard = require('./scorecard');
 
 // Gutter Game Feature Test
 
-test('GutterGame Total score is 0', () => {
+test('GutterGame Total score is 0, No frames remain', () => {
   var scorecard = new Scorecard();
 
-  scorecard.bowl(0,0)
-  scorecard.bowl(0,0);
-  scorecard.bowl(0,0);
-  scorecard.bowl(0,0);
-  scorecard.bowl(0,0);
-  scorecard.bowl(0,0);
-  scorecard.bowl(0,0);
-  scorecard.bowl(0,0);
-  scorecard.bowl(0,0);
-  scorecard.bowl(0,0);
-  scorecard.bowl(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
 
-  expect((scorecard.Total).toBe(0));
+  expect(scorecard.total).toBe(0);
+  expect(scorecard.frames).toBe(0);
+});
+
+test('GutterGame bar one pin, Total score is 1, No frames remain', () => {
+  var scorecard = new Scorecard();
+
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,0);
+  scorecard.frame(0,1);
+
+  expect(scorecard.total).toBe(1);
+  expect(scorecard.frames).toBe(0);
+
+});
+
+test('frame function reduces frames attribute by one', () => {
+  var scorecard = new Scorecard();
+
+  scorecard.frame(0,0);
+
+  expect(scorecard.frames).toBe(9);
 });
